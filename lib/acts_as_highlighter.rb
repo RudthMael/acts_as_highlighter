e9e65f82d1c085aa83d94175cee19db3eeae697c
Konstantin Ivanov <ivanov.konstantin@logrus.org.ru>
2013-05-26 19:47:55 +0400

    update to 0.0.2

--- lib/acts_as_highlighter.rb
+++ lib/acts_as_highlighter.rb
@@ -8,9 +8,36 @@ module ActsAsHighlighter
 
 		attr_accessor :default_syntax
 
-		def code_block( content, syntax = default_syntax )
-			# как добавить эскейпинг входной строки?
-			"<pre class='brush: #{syntax};'>#{content}</pre>".html_safe
+		def code_block( content, *opts )
+			params = {
+				:brush => @default_syntax,
+				:auto_links => "true",
+				:class_name => '',
+				:collapse => "false",
+				:first_line => "1",
+				:highlight => "null",
+				:html_script => "false",
+				:smart_tabs => "true",
+				:tab_size => "4",
+				:toolbar => "true"
+			}
+
+			# сохраняем значения по умолчанию
+			tmp = params.clone
+
+			if opts[0].is_a? String
+				params[:brush] = opts[0]
+				params.merge!(opts[1]) if opts[1].is_a? Hash
+			elsif opts[0].is_a? Hash
+				params.merge!(opts[0])
+			end
+
+			# разгрузка html-кода от дефолтных опций
+			params.delete_if{ |k,v| ( tmp[k] == v ) and ( k != :brush ) }
+
+			parms_str = params.map{ |k,v| "#{k}: #{v}; " }.join.chop
+					
+			"<pre class=\"#{parms_str}\">#{content}</pre>".html_safe
 		end
 
 	end
@@ -21,7 +48,7 @@ module ActsAsHighlighter
 end
 
 class String
-	def highlight( syntax = ActsAsHighlighter.default_syntax )
-		ActsAsHighlighter.code_block( self, syntax )
+	def highlight( *params )
+		ActsAsHighlighter.code_block( self, *params )
 	end
 end
